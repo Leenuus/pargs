4dcfb769ec3d48c7592360354b060dd00f35a1b2
Leenuus <linusrach24@gmail.com>
2024-09-16 14:53:48 +0800

    Add debug flag

--- pargs.pl
+++ pargs.pl
@@ -14,15 +14,17 @@ our $reset = color('reset');
 my $SEP;
 my $n;
 my $replace;
+my $debug;
 
 GetOptions(
     'delimiter=s' => \$SEP,
     'n=i'         => \$n,
     'I=s'         => \$replace,
+    'D'           => \$debug,
 );
 
-$replace = '%' unless defined($replace);
-say STDERR qq(${red}Debug: use replace string `$replace`$reset);
+$replace = '{}' unless defined($replace);
+say STDERR qq(${red}Debug: use replace string `$replace`$reset) if $debug;
 
 die qq(${red}n should not be 0$reset) if defined($n) && $n == 0;
 
@@ -31,18 +33,19 @@ my @args = ();
 if ( defined($SEP) ) {
     my @lines = <STDIN>;
     $_ = join "\n", @lines;
-    say STDERR qq(${red}Debug: use separator `$SEP`$reset);
+    say STDERR qq(${red}Debug: use separator `$SEP`$reset) if $debug;
     push @args, split( $SEP, $_ );
 }
 else {
-    say STDERR qq(${red}Debug: use default separator `\\n`$reset);
+    say STDERR qq(${red}Debug: use default separator `\\n`$reset) if $debug;
     chomp( @args = <STDIN> );
 }
 
 my @cmd = @ARGV;
 if ( @cmd == 0 ) {
     push @cmd, 'echo';
-    say STDERR qq(${red}Debug: No command given, fallback to `echo`$reset);
+    say STDERR qq(${red}Debug: No command given, fallback to `echo`$reset)
+      if $debug;
 }
 
 # NOTE:
@@ -54,7 +57,7 @@ if ( my $num = grep { $_ eq $replace } @cmd ) {
     while ( @_ = splice( @args, 0, $num ) ) {
         my @to_run = @cmd;
         @to_run = map { $_ eq $replace ? shift @_ : $_; } @to_run;
-        say STDERR qq(${red}Debug: run command: `@{to_run}`$reset);
+        say STDERR qq(${red}Debug: run command: `@{to_run}`$reset) if $debug;
         start( \@to_run );
     }
     wait;
@@ -63,13 +66,13 @@ elsif ( defined $n ) {
     while ( @_ = splice( @args, 0, $n ) ) {
         my @to_run = @cmd;
         push @to_run, @_;
-        say STDERR qq(${red}Debug: run command: `@{to_run}`$reset);
+        say STDERR qq(${red}Debug: run command: `@{to_run}`$reset) if $debug;
         start( \@to_run );
     }
     wait;
 }
 else {
     push @cmd, @args;
-    say STDERR qq(${red}Debug: run command: `@{cmd}`$reset);
+    say STDERR qq(${red}Debug: run command: `@{cmd}`$reset) if $debug;
     exec @cmd;
 }
